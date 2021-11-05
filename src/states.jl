e75adced9631cba403de528216d0645a167c4646
Herman Brunborg <herman@brunborg.com>
2021-11-05 10:52:06 +0100

    Updated finding of analytical expressions

--- src/states.jl
+++ src/states.jl
@@ -1,13 +1,16 @@
 using DelimitedFiles # for writing the csv file
 using DataStructures # for DefaultDict
 
-N = 2
+L = 2
+N = L ^ 2
 
-spins = Iterators.product(fill((-1,1),N^2)...)
-spins = map(x -> reshape(collect(x), (2,2)), spins)[:]
+spins = Iterators.product(fill((-1,1),N)...)
+spins = map(x -> reshape(collect(x), (L,L)), spins)[:]
 
 state_summary = []
 degeneracies  = DefaultDict(0)
+
+Sigmas = [0, 0, 0, 0]
 
 for spin in spins
     positive_spins = count(i -> i == 1, spin)
@@ -15,6 +18,11 @@ for spin in spins
     E_v = spin .* circshift(spin, (0,1))
     E_s = -sum(E_h + E_v)
     M_s = sum(spin)
+
+    Sigmas[1] += E_s
+    Sigmas[2] += E_s ^ 2
+    Sigmas[3] += abs(M_s)
+    Sigmas[4] += M_s ^ 2
 
     degeneracies[E_s] += 1
 
@@ -27,3 +35,9 @@ end
 
 pushfirst!(state_summary, ["1+ spins", "E(s)", "M(s)", "Degeneracies"])
 writedlm("output/state_summary.csv",  state_summary, ',')
+
+
+println("⟨ϵ⟩:   ", Sigmas[1] / N)
+println("⟨ϵ²⟩:  ", Sigmas[2] / (N ^ 2))
+println("⟨|m|⟩: ", Sigmas[3] / N)
+println("⟨m²⟩:  ", Sigmas[4] / (N ^ 2))
