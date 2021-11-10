7dae0513b67f1f262b0d8b1d0e17878d8aa0fecb
Herman Brunborg <herman@brunborg.com>
2021-11-10 16:48:42 +0100

    Added exact analyticals

--- src/analytical_ising_model.py
+++ src/analytical_ising_model.py
@@ -26,11 +26,12 @@ class AnalyticalIsingModel:
         return self.expected_E / self._N
 
     @property
-    def expected_absolute_m(self):
-        return (
-            np.sum(np.abs(self._states["M(s)"]) * self.p(self._states["E(s)"]))
-            / self._N
-        )
+    def expected_magnitization(self):
+        return np.sum(self._states["M(s)"] * self.p(self._states["E(s)"]))
+
+    @property
+    def expected_abs_M(self):
+        return np.sum(np.abs(self._states["M(s)"]) * self.p(self._states["E(s)"]))
 
     @property
     def expected_E_squared(self):
@@ -47,11 +48,41 @@ class AnalyticalIsingModel:
         )
 
 
-if __name__ == "__main__":
-    aim = AnalyticalIsingModel("output/state_summary.csv", temperature=10)
-    print(aim.expected_E)
-    print(aim.expected_epsilon)
-    print(aim.expected_epsilon_squared)
+class Superanalytical:
+    def __init__(self, filename: str, temperature: float, L=2):
+        self._L = L
+        self._N = L ** 2
+        self._T = temperature
+        self._beta = 1 / self._T  # Boltzman-constant?
+        self._states = pd.read_csv(filename)
 
-    print(aim.expected_absolute_m)
-    print(aim.expected_m_squared)
+    def p(self, E_s):
+        return 1 / self.Z * np.exp(-self._beta * E_s)
+
+    @property
+    def Z(self):
+        return 4 * np.cosh(8 * self._beta) + 12
+
+    @property
+    def expected_E(self):
+        return -(8 * np.sinh(8 * self._beta)) / (np.cosh(8 * self._beta) + 3)
+
+    @property
+    def expected_abs_M(self):
+        return (2 * np.exp(8 * self._beta) + 4) / (np.cosh(8 * self._beta) + 3)
+
+
+if __name__ == "__main__":
+    aim = AnalyticalIsingModel("output/state_summary.csv", temperature=1)
+    sup = Superanalytical("output/state_summary.csv", temperature=1)
+    print(aim.Z - sup.Z)
+    print(aim.expected_E - sup.expected_E)
+    print(aim.expected_abs_M - sup.expected_abs_M)
+    #  print(aim.expected_E)
+    #  print(aim.expected_epsilon)
+    #  print(aim.expected_epsilon_squared)
+    #
+    #  print(aim.expected_magnitization)
+    #
+    #  print(aim.expected_absolute_m)
+    #  print(aim.expected_m_squared)
