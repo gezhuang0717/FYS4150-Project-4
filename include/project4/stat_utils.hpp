45b7c247ea4ed503d1a75dd0f53ebff4f88b4a0b
sim-hal <simon.halstensen@gmail.com>
2021-11-15 16:33:56 +0100

    added missing file

---
+++ include/project4/stat_utils.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <map>
+#include <functional>
+
+namespace stat_utils{
+    void print_map(std::map<int, float> p);
+    float expected_value(std::map<int, float> p);
+    float expected_value(std::map<int, float> p, std::function<int(int)> f);
+    float expected_value(int* samples, int N);
+    float expected_value(int* samples, int N, std::function<int(int)> f);
+    std::map<int, float> distribution(int* samples, int N);
+    std::map<int, float> distribution(int* samples, int N, std::function<int(int)> f);
+}
+
