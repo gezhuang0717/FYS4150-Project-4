53e76a0c940bec23214d6f3a9b1d7445615cc574
Herman Brunborg <herman@brunborg.com>
2021-11-15 16:53:46 +0100

    Changed floats to doubles

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,8 @@ void find_burn_in_time(int N, int L=20, double T=1){
     vector<double> expected_E_burn_in;
     vector<double> expected_M_burn_in;
     for (int burn_in=0; burn_in<N; burn_in+=max(1, N/100)){
-        map<int, float> buckets_E = stat_utils::distribution(sampled_E + burn_in, N - burn_in);
-        map<int, float> buckets_M = stat_utils::distribution(sampled_M + burn_in, N - burn_in);
+        map<int, double> buckets_E = stat_utils::distribution(sampled_E + burn_in, N - burn_in);
+        map<int, double> buckets_M = stat_utils::distribution(sampled_M + burn_in, N - burn_in);
         expected_E_burn_in.push_back(stat_utils::expected_value(buckets_E));
         expected_M_burn_in.push_back(stat_utils::expected_value(buckets_M));
         burn_in_time.push_back(burn_in);
@@ -61,18 +61,18 @@ void produce_distributions(const int iters, const int L, double T, int burn_in_t
         sampled_magnetization_abs[i] = abs(model.get_magnetization());
     }
     auto scale = [N](int x){return x / N;};
-    map<int, float> buckets_epsilon = stat_utils::distribution(sampled_energy, iters, scale);
-    map<int, float> buckets_m_abs = stat_utils::distribution(sampled_magnetization_abs, iters, scale);
+    map<int, double> buckets_epsilon = stat_utils::distribution(sampled_energy, iters, scale);
+    map<int, double> buckets_m_abs = stat_utils::distribution(sampled_magnetization_abs, iters, scale);
 
     auto square = [](int x){return x * x;};
-    float expected_epsilon = stat_utils::expected_value(buckets_epsilon);
-    float expected_m_abs = stat_utils::expected_value(buckets_m_abs);
-    float expected_energy = stat_utils::expected_value(sampled_energy, iters);
-    float expected_energy_sq = stat_utils::expected_value(sampled_energy, iters, square);
-    float expected_magnetization_abs = stat_utils::expected_value(sampled_magnetization_abs, iters);
-    float expected_magnetization_sq = stat_utils::expected_value(sampled_magnetization_abs, iters, square);
-    float c_v = (1. / N) * (1. / (T * T)) * (expected_energy_sq - expected_energy * expected_energy);
-    float chi = (1. / N) * (1. / T) * (expected_magnetization_sq - expected_magnetization_abs * expected_magnetization_abs);
+    double expected_epsilon = stat_utils::expected_value(buckets_epsilon);
+    double expected_m_abs = stat_utils::expected_value(buckets_m_abs);
+    double expected_energy = stat_utils::expected_value(sampled_energy, iters);
+    double expected_energy_sq = stat_utils::expected_value(sampled_energy, iters, square);
+    double expected_magnetization_abs = stat_utils::expected_value(sampled_magnetization_abs, iters);
+    double expected_magnetization_sq = stat_utils::expected_value(sampled_magnetization_abs, iters, square);
+    double c_v = (1. / N) * (1. / (T * T)) * (expected_energy_sq - expected_energy * expected_energy);
+    double chi = (1. / N) * (1. / T) * (expected_magnetization_sq - expected_magnetization_abs * expected_magnetization_abs);
 
     cout << "Sampled distribution of epsilon" << endl;
     stat_utils::print_map(buckets_epsilon);
--- src/utils.cpp
+++ src/utils.cpp
@@ -4,39 +4,39 @@
 
 int I(int x) {return x;}
 
-void stat_utils::print_map(std::map<int, float> p){
+void stat_utils::print_map(std::map<int, double> p){
     for (auto const& bucket : p){
         std::cout << bucket.first << ": " << bucket.second << std::endl;
     }
 }
 
 
-float stat_utils::expected_value(std::map<int, float> p, std::function<int(int)> f){
-    float expected_value = 0.;
+double stat_utils::expected_value(std::map<int, double> p, std::function<int(int)> f){
+    double expected_value = 0.;
     for (auto const& bucket : p){
         expected_value += f(bucket.first) * bucket.second;
     }
     return expected_value;
 }
 
-float stat_utils::expected_value(std::map<int, float> p){
+double stat_utils::expected_value(std::map<int, double> p){
     return expected_value(p, I);
 }
 
-float stat_utils::expected_value(int *samples, int N, std::function<int(int)> f){
+double stat_utils::expected_value(int *samples, int N, std::function<int(int)> f){
     int sum = 0;
     for (int i = 0; i < N; i++){
         sum += f(samples[i]);
     }
-    return (float)sum / N;
+    return (double)sum / N;
 }
 
-float stat_utils::expected_value(int *samples, int N){
+double stat_utils::expected_value(int *samples, int N){
     return expected_value(samples, N, I);
 }
 
-std::map<int, float> stat_utils::distribution(int *samples, int N, std::function<int(int)> f){
-    std::map<int, float> buckets;
+std::map<int, double> stat_utils::distribution(int *samples, int N, std::function<int(int)> f){
+    std::map<int, double> buckets;
     for (int i = 0; i < N; i++){
         int sample = f(samples[i]);
         buckets.emplace(sample, 0);
@@ -45,6 +45,6 @@ std::map<int, float> stat_utils::distribution(int *samples, int N, std::function
     return buckets;
 }
 
-std::map<int, float> stat_utils::distribution(int *samples, int N){
+std::map<int, double> stat_utils::distribution(int *samples, int N){
     return distribution(samples, N, I);
 }
