7a7cc6aad253546d3da80ed67e90421a30dbef34
carlfre <carlfrederiknk@gmail.com>
2021-11-17 16:27:00 +0100

    seed things

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,8 @@
 
 using namespace std;
 
-void sample(vector<int> &sampled_energy, vector<int> &sampled_magnetization_abs, const int iters, int L, double T, int burn_in_time = 1000, bool random_spins = true){
-    IsingModel model(L, T, random_spins);
+void sample(vector<int> &sampled_energy, vector<int> &sampled_magnetization_abs, const int iters, int L, double T, int seed, int burn_in_time = 1000, bool random_spins = true){
+    IsingModel model(L, T, random_spins, seed);
     for (int i = -burn_in_time; i < iters; i++){
         model.metropolis();
         if (i < 0) continue;
@@ -31,12 +31,12 @@ void sample(vector<int> &sampled_energy, vector<int> &sampled_magnetization_abs,
  * @param T temperature
  * @param burn_in_time Number of iterations to discard when producing estimates 
  */
-void write_distributions(const int iters, const int L, double T, string filename_epsilon, string filename_m_abs, int burn_in_time = 1000){ // note we don't have any indications on what the burn_in_time is yet
-    IsingModel model(L, T, true);
+void write_distributions(const int iters, const int L, double T, int seed, string filename_epsilon, string filename_m_abs, int burn_in_time = 1000){ // note we don't have any indications on what the burn_in_time is yet
+    // IsingModel model(L, T, true, seed); // unused?
     const int N = L * L;
     vector<int> sampled_energy;
     vector<int> sampled_magnetization_abs;
-    sample(sampled_energy, sampled_magnetization_abs, iters, L, T);
+    sample(sampled_energy, sampled_magnetization_abs, iters, L, T, seed);
     auto scale = [N](int x){return (double)x / N;};
     map<double, double> buckets_epsilon = stat_utils::distribution(sampled_energy, iters, scale);
     map<double, double> buckets_m_abs = stat_utils::distribution(sampled_magnetization_abs, iters, scale);
@@ -72,23 +72,23 @@ double &expected_epsilon, double &expected_m_abs, double &c_v, double &chi){
  * @param T temperature
  * @param outfile csv-file to write results
  */
-void write_values_to_file(int L, double T, ofstream &outfile){
+void write_values_to_file(int L, double T, int seed, ofstream &outfile){
     int sample_size = 10000;
     vector<int> sampled_energy;
     vector<int> sampled_magnetization_abs;
-    sample(sampled_energy, sampled_magnetization_abs, sample_size, L, T, 1000);
+    sample(sampled_energy, sampled_magnetization_abs, sample_size, L, T, seed, 1000);
     double expected_epsilon, expected_m_abs, c_v, chi;
     values(sampled_energy, sampled_magnetization_abs, sample_size, L, T, expected_epsilon, expected_m_abs, c_v, chi);
     outfile << T << "," << expected_epsilon << "," << expected_m_abs << "," << c_v << "," << chi << endl;
 }
 
-void find_burn_in_time(int N, int L=20, double T=1, bool random_spins=true){
+void find_burn_in_time(int N, int L, double T, int seed, bool random_spins=true){
     vector<int> sampled_E;
     vector<int> sampled_M_abs;
     vector<double> E_avg;
     vector<double> M_abs_avg;
     vector<int> N_vector;
-    sample(sampled_E, sampled_M_abs, N, L, T, 0, random_spins);
+    sample(sampled_E, sampled_M_abs, N, L, T, seed, 0, random_spins);
     for (int i=0; i<N; i++){
         double expected_eps, expected_m_abs, c_v, chi;
         values(sampled_E, sampled_M_abs, i+1, L, T, expected_eps, expected_m_abs, c_v, chi);
@@ -122,12 +122,13 @@ void find_burn_in_time(int N, int L=20, double T=1, bool random_spins=true){
  * @return int number of samples needed for convergence
  */
 int test2x2(){
+    int seed = 3875623;
     const double tol = 1e-2;
     vector<int> sampled_energy;
     vector<int> sampled_magnetization_abs;
     double T = 1.;
     int max_sample_size = 10000;
-    sample(sampled_energy, sampled_magnetization_abs, max_sample_size, 2, T, 0);
+    sample(sampled_energy, sampled_magnetization_abs, max_sample_size, 2, T, seed, 0);
     double expected_epsilon, expected_m_abs, c_v, chi;
     double analytical_expected_epsilon = -1.99598208593669, analytical_expected_m_abs = 0.9986607327485997, analytical_c_v = 0.032082331864287994, analytical_chi = 0.004010739516227435;
     int using_sample_size = 1;
@@ -158,9 +159,11 @@ int main(){
     // outfile.close();
     // write_distributions(10000, 20, 1, "output/distribution_epsilon_L=20_T=1.csv", "output/distribution_m_abs_L=20_T=1.csv");
     // write_distributions(10000, 20, 2.4, "output/distribution_epsilon_L=20_T=2.4.csv", "output/distribution_m_abs_L=20_T=2.4.csv");
-    find_burn_in_time(2000, 20, 1, false);
-    find_burn_in_time(1000, 20, 1, true);
-    find_burn_in_time(6000, 20, 2.4, false);
-    find_burn_in_time(6000, 20, 2.4, true);
+
+    int seed = 456788;
+    find_burn_in_time(2000, 20, 1, seed++, false);
+    find_burn_in_time(1000, 20, 1, seed++, true);
+    find_burn_in_time(6000, 20, 2.4, seed++, false);
+    find_burn_in_time(6000, 20, 2.4, seed++, true);
     return 0;
 }
