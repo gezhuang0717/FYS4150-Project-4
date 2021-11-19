f080cb13aa1e02931b35c6b8b60ccd41491393c3
sim-hal <simon.halstensen@gmail.com>
2021-11-19 20:31:45 +0100

    small function to estimate T_inf

--- src/plot.py
+++ src/plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import scipy.stats as sts
 def plot_burn_in_time():
     filenames = ["burn_in_T_1.000000_nonrandom.csv",
                  "burn_in_T_1.000000_random.csv",
@@ -43,10 +43,24 @@ def plot_values_and_print_max():
         print(f"Look between temperatures {df.loc[min(argmax_C_v, armmax_chi) - 1]['T']} and {df.loc[max(argmax_C_v, armmax_chi) + 1]['T']}")
         plt.show()
 
+def estimate_T_inf():
+    y = []
+    x = []
+    for L in range(20, 120, 20):
+        df = pd.read_csv(f"output/values_zoom_L={L}.csv")
+        argmax_C_v = df.C_v.idxmax()
+        armmax_chi = df.chi.idxmax()
+        y.append((df.loc[argmax_C_v]['T'] + df.loc[argmax_C_v]['T']) / 2)
+        x.append(1 / L)
+    print(sts.linregress(x, y).intercept)
+    
+
+
 def main():
     #plot_burn_in_time()
     #plot_probability_distribution()
-    plot_values_and_print_max()
+    # plot_values_and_print_max()
+    estimate_T_inf()
 
 if __name__ == "__main__":
     main()
